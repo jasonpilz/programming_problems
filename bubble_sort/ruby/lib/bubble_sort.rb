7b71917e89b2a12d302ac8f48d2931538ca665c5
Jason Pilz <jasonpilz@gmail.com>
2016-04-21 20:46:45 -0600

    first pass at bubble_sort complete

--- bubble_sort/ruby/lib/bubble_sort.rb
+++ bubble_sort/ruby/lib/bubble_sort.rb
@@ -1,3 +1,12 @@
 class BubbleSort
-
+  def sort(array)
+    array.count.times do
+      array.count.pred.times do |x|
+        if array[x + 1]  < array[x]
+          array[x], array[x + 1] = array[x + 1], array[x]
+        end
+      end
+    end
+    array
+  end
 end
--- bubble_sort/ruby/test/bubble_sort_test.rb
+++ bubble_sort/ruby/test/bubble_sort_test.rb
@@ -2,5 +2,35 @@ require_relative '../lib/bubble_sort.rb'
 require 'minitest/autorun'
 
 class BubbleSortTest < Minitest::Test
+  def test_it_can_sort_empty_collection
+    bubble_sort = BubbleSort.new
 
+    sorted = bubble_sort.sort([])
+
+    assert_equal [], sorted
+  end
+
+  def test_it_can_sort_already_sorted_collection
+    bubble_sort = BubbleSort.new
+
+    sorted = bubble_sort.sort([1, 2])
+
+    assert_equal [1, 2], sorted
+  end
+
+  def test_it_can_sort_3_element_collection
+    bubble_sort = BubbleSort.new
+
+    sorted = bubble_sort.sort([2, 1, 3])
+
+    assert_equal [1, 2, 3], sorted
+  end
+
+  def test_it_can_sort_7_element_collection
+    bubble_sort = BubbleSort.new
+
+    sorted = bubble_sort.sort([4, 3, 2, 1, 5, 7, 6])
+
+    assert_equal [1, 2, 3, 4, 5, 6, 7], sorted
+  end
 end
